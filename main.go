9cfd3b657c26ec6859ef13799c46b706722bb86b
Aishraj <aishraj@users.noreply.github.com>
2015-07-25 12:07:00 +0530

    Adding places static driver

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
+	http.HandleFunc("/api/v1/places.json", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "places.json")
+	})
 	log.Println("Listening on port 4000....")
 	http.ListenAndServe(":4000", nil)
 }
