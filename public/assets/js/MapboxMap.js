a115baa2d1f8739133fb8c5c7cb5cabee6932045
Aishraj <aishraj@users.noreply.github.com>
2015-07-27 12:07:50 +0545

    Using react to manage state

--- public/assets/js/MapboxMap.js
+++ public/assets/js/MapboxMap.js
@@ -1,27 +1,49 @@
-var React = require('react');
+var React = require('react'),
+$ = require('jquery');
 // Assuming Mapbox/Leaflet is already exposed as `L`
 
 var MapboxMap = React.createClass({
+  loadCoordinatesFromServer: function(map, L) {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(coordinate) {
+        if (this.state.coordinate.x === 0 && this.state.coordinate.y === 0 && $.isEmptyObject(this.state.polyline)) {
+          var polyline  = L.polyline([]).addTo(map);
+          polyline.addLatLng(L.latLng(coordinate.x,coordinate.y));
+          this.setState({polyline: polyline });
+        }
+        this.setState({coordinate: coordinate});
+        if (!$.isEmptyObject(this.state.polyline)) {
+          this.state.polyline.addLatLng(L.latLng(this.state.coordinate.x,this.state.coordinate.y));
+        }
+        map.setView([coordinate.x,coordinate.y], 3);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {coordinate: {x: 0, y:0}, polyline: {}};
+  },
   componentDidMount: function(argument) {
     var props = this.props;
-
     var mapId = props.mapId || props.src || "mapbox.streets";
-
     var options = {};
-    options.accessToken = 'pk.eyJ1IjoiYXJkYWhhbCIsImEiOiJseFQyTWlrIn0.zX_ANNp_k20-iC-6VmbilA';
-    var ownProps = ['mapId', 'onMapCreated', 'dataUrl'];
+    var ownProps = ['mapId','pollInterval','url'];
     for (var k in props) {
       if (props.hasOwnProperty(k) && ownProps.indexOf(k) === -1) {
         options[k] = props[k];
       }
     }
-
     var map = L.mapbox.map(this.getDOMNode(), mapId, options);
-    var dataUrl = this.props.dataUrl;
-
-    if (this.props.onMapCreated) {
-      this.props.onMapCreated(map, L, dataUrl);
-    }
+    var that = this;
+    this.loadCoordinatesFromServer(map, L);
+    setInterval(function(){
+      that.loadCoordinatesFromServer(map,L);
+    }, this.props.pollInterval);
   },
 
   render: function() {
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,62 +1,23 @@
 var React = require('react'),
-    MapboxMap = require('./MapboxMap');
+MapboxMap = require('./MapboxMap');
 
 var App = React.createClass({
   render: function() {
-    /*
-     * Attributes:
-     * - `mapId` (or `src` -- for semantics)
-     *        -- that "id or url or tilejson" parameter of L.mapbox.map
-     * - `onMapCreated(map, L)`
-     *        -- a special callback allowing you to further set up your map
-     * - any of the L.mapbox.map and L.Map options, see
-     *   https://www.mapbox.com/mapbox.js/api/v2.1.6/l-mapbox-map/ and
-     *   https://www.mapbox.com/mapbox.js/api/v2.1.6/l-map-class/
-     *   for more info.
-     */
-     var onMapCreated = function(map, L, dataUrl) {
-       var polyline = L.polyline([]).addTo(map);
-       var pointsAdded = 0;
-       var add = function(that) {
-
-          // `addLatLng` takes a new latLng coordinate and puts it at the end of the
-          // line. You optionally pull points from your data or generate them. Here
-          // we make a sine wave with some math.
-          polyline.addLatLng(
-              L.latLng(
-                  Math.cos(pointsAdded / 20) * 30,
-                  pointsAdded));
-          console.log("the dataUrl insides is " + that.dataUrl);
-          // Pan the map along with where the line is being added.
-          map.setView([0, pointsAdded], 3);
-
-          // Continue to draw and pan the map by calling `add()`
-          // until `pointsAdded` reaches 360.
-          if (++pointsAdded < 360) window.setTimeout( function() {
-            add(that);
-          }, 100);
-        };
-        add(this);
-     };
-
-
-
-
     return (
       <div className="container">
         <MapboxMap
-          mapId="mapbox.streets"
-          dataUrl="/api/v1/places.json"
-          onMapCreated={onMapCreated}
+          url="/api/v1/places.json"
+          accessToken="pk.eyJ1IjoiYXJkYWhhbCIsImEiOiJseFQyTWlrIn0.zX_ANNp_k20-iC-6VmbilA"
           zoomControl={true}
-          center={[0, 0]} zoom={3}/>
+          pollInterval={2000}
+          center={[0, 0]} zoom={4}/>
       </div>
     );
   }
 });
 
 App.start = function () {
-    React.render(<App/>, document.getElementById('content'));
+  React.render(<App/>, document.getElementById('content'));
 };
 
 module.exports = window.App = App;
