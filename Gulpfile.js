77a01a9401ff23ae1c98a5be66cb7b864d14ddf7
Aishraj <aishraj@users.noreply.github.com>
2015-07-30 08:44:36 +0545

    Minifying js files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,44 +1,44 @@
 var gulp       = require('gulp'),
-
-    browserify = require('gulp-browserify'),
-    concat     = require('gulp-concat'),
-    imagemin   = require('gulp-imagemin');
-    del = require('del');
+browserify = require('gulp-browserify'),
+concat     = require('gulp-concat'),
+imagemin   = require('gulp-imagemin'),
+del = require('del'),
+uglify = require('gulp-uglify');
 
 gulp.task('styles', function () {
 
-    gulp.src(['public/assets/css/typography.css', 'public/assets/css/styles.css'])
-        .pipe(concat('styles.css'))
-        .pipe(gulp.dest('./public/build/'));
+  gulp.src(['public/assets/css/typography.css', 'public/assets/css/styles.css'])
+  .pipe(concat('styles.css'))
+  .pipe(gulp.dest('./public/build/'));
 
 });
 
 gulp.task('scripts', function () {
 
-    gulp.src(['public/assets/js/app.js'])
-        .pipe(browserify({
-            debug: true,
-            transform: [ 'babelify' ]
-        }))
-        .pipe(gulp.dest('public/build/'));
+  gulp.src(['public/assets/js/app.js'])
+  .pipe(browserify({
+    debug: true,
+    transform: [ 'babelify' ]
+  })).pipe(uglify())
+  .pipe(gulp.dest('public/build/'));
 
 });
 
 gulp.task('images', function () {
 
-    gulp.src(['public/assets/img/**/*.png', 'public/assets/img/**/*.gif'])
-        .pipe(imagemin())
-        .pipe(gulp.dest('public/build/img/'));
+  gulp.src(['public/assets/img/**/*.png', 'public/assets/img/**/*.gif'])
+  .pipe(imagemin())
+  .pipe(gulp.dest('public/build/img/'));
 
 });
 
 gulp.task('dev', function () {
 
-    gulp.run('build');
+  gulp.run('build');
 
-    gulp.watch('public/assets/js/**/*.js', [ 'scripts' ]);
-    gulp.watch('public/assets/css/**/*.css', [ 'styles' ]);
-    gulp.watch('public/assets/img/**/*', [ 'images' ]);
+  gulp.watch('public/assets/js/**/*.js', [ 'scripts' ]);
+  gulp.watch('public/assets/css/**/*.css', [ 'styles' ]);
+  gulp.watch('public/assets/img/**/*', [ 'images' ]);
 
 });
 
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # vTraceRoute - A simple traceroute visualizer.
 
 vTraceRoute is a simple tool that allows visualization of IP address's Geolocation points on an OpenStreetMap (Mapbox) map.
+Tools used:
+1. Go programming language
+2. Reactjs
+3. Mapbox.js (leaflet)
 
 #Getting started
 First install `npm` and `go` programming language SDK. Once you've got it setup perform the following in sequence:
@@ -9,13 +13,13 @@ First install `npm` and `go` programming language SDK. Once you've got it setup 
 3. `npm install`
 4. `gulp build`
 5. `go install`
-6. `$GOPATH/bin/vtracerroute <server you'd like to ping>`
+6. `$GOPATH/bin/vtracerroute -host <server you'd like to ping>`
 7. Navigate to localhost:4000/ to view the paths that your packet took to reach the host specified in step 6 above.
 
 #Disclaimer
 This is my first react.js project and hence the quality of the JS (and JSX) code might be mediocre at best. In case you feel that things can be improved, go ahead and send a pull request.
 
 #TODO
-1. Minify the JS files
-2. Make paths traceable
-3. Figure out if the React DOM is having any benefit (since leaflet.js renders the map component)
+1. Make paths traceable in terms of direction.
+2. Figure out if the React DOM is having any benefit (since leaflet.js renders the map component)
+3. Stop redrawing once you hit a certain hops / certain number of fetch calls.
